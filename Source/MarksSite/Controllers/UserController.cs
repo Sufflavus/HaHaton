d0d7c1bbd1b22ffdedbce9193c72c52a70322c4d
Nerielle <nelya.iva@gmail.com>
2015-09-30 15:55:37 +0300

    +userController

---
+++ Source/MarksSite/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using MarksDal;
+using MarksSite.Models;
+
+namespace MarksSite.Controllers
+{
+    public class UserController : Controller
+    {
+        private static readonly Repository repository = new Repository();
+        //
+        // GET: /User/
+
+        public ActionResult List(int userId)
+        {
+            var users = repository.GetUsers().Where(x => x.Id != userId && !string.IsNullOrEmpty(x.FirstName));
+            var userViewModels = users.Select(x => new UserViewModel()
+            {
+                Email = string.Empty,
+                FullName = x.FirstName,
+                DomainName = string.Empty,
+                IsManager = (bool)x.IsManager,
+                Department = string.Empty,
+                JobPosition = string.Empty,
+            }).ToList();
+
+            return View(userViewModels);
+        }
+    }
+}
